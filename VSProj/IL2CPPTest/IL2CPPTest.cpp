7bbf0296924fc766a714840ed0d2347de630a6a9
liiir1985 <liiir1985@hotmail.com>
2017-12-01 11:27:47 +0800

    Runnable Test Project completed

--- VSProj/IL2CPPTest/IL2CPPTest.cpp
+++ VSProj/IL2CPPTest/IL2CPPTest.cpp
@@ -2,10 +2,14 @@
 //
 
 #include "stdafx.h"
-
+#include "il2cpp-api.h"
+#include "AssemblyU2DCSharp_TestLib_Main3706940067MethodDeclarations.h"
 
 int main()
 {
-    return 0;
+	il2cpp_init("My domain");
+	Main_TestGC_m1651579141(NULL, NULL);
+
+	return 0;
 }
 
